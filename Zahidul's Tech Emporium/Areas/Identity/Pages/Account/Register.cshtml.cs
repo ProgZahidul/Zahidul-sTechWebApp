bebabc2bafa4a3f693d31f7bf699d99ab5ffa204
saikat hoque <sayedzahidulhoquesaikat@gmail.com>
2024-06-26 13:08:16 +0600

    Add register other field edit Manage _Layout

--- Zahidul's Tech Emporium/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Zahidul's Tech Emporium/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -108,6 +108,13 @@ namespace Zahidul_s_Tech_Emporium.Areas.Identity.Pages.Account
 
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+            [Required]
+            public string Name { get; set; }
+            public string? Address { get; set; }
+            public string? City { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
+
         }
 
 
@@ -145,7 +152,13 @@ namespace Zahidul_s_Tech_Emporium.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Name = Input.Name;
+                user.Address = Input.Address;
+                user.City = Input.City;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
                 var result = await _userManager.CreateAsync(user, Input.Password);
+
 
                 if (result.Succeeded)
                 {
